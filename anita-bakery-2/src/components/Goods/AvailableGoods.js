65f742b3318858b3de7aea6e536f8994cc325827
AndrewSimons3 <simdrew07@gmail.com>
2021-12-08 08:08:47 -0600

    fetching goods data from server

--- anita-bakery-2/src/components/Goods/AvailableGoods.js
+++ anita-bakery-2/src/components/Goods/AvailableGoods.js
@@ -1,57 +1,46 @@
+import { useEffect, useState } from 'react';
 import classes from './AvailableGoods.module.css'
 import Card from '../UI/Card';
 import GoodItem from './GoodItem/GoodItem';
 
-const DUMMY_GOODS = [
-  {
-    id: 'm1',
-    name: 'Chocolate Chip Cookies',
-    description: 'Classic and best seller',
-    price: 2.00,
-  },
-  {
-    id: 'm2',
-    name: 'Lemon Ricotta',
-    description: 'made with love',
-    price: 2.50,
-  },
-  {
-    id: 'm3',
-    name: 'Red Velvet Cheesecake Brownies',
-    description: 'Will you be mine?',
-    price: 5,
-  },
-  {
-    id: 'm4',
-    name: 'Pumpkin Chocolate Chip Cookies',
-    description: 'Spooky good taste!',
-    price: 2.50,
-  },
-  {
-    id: 'm5',
-    name: 'Almond Chocolate Cheesecake',
-    description: 'Nuttylicious',
-    price: 5,
-  },
-  {
-    id: 'm6',
-    name: 'Strawberry Ricotta Cake Bars',
-    description: 'mmmm...lets get fruity',
-    price: 4,
-  },
-];
 
 
 const AvailableGoods = () => {
-  const goodsList = DUMMY_GOODS.map((good) =>  (
-    <GoodItem 
-      id={good.id}
-      key={good.id} 
-      name={good.name} 
-      description={good.description} 
-      price={good.price}
-      />
-  ));
+  const [goods, setGoods] = useState([]);
+
+  useEffect(() => {
+		const fetchGoods = async () => {
+			const response = await fetch(
+				'https://react-http-516cc-default-rtdb.firebaseio.com/goods.json'
+			);
+
+			const responseData = await response.json();
+
+			const loadedGoods = [];
+
+			for (const key in responseData) {
+				loadedGoods.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+      }
+      
+      setGoods(loadedGoods);
+    }
+    fetchGoods();
+  }, []);
+  
+    const goodsList = goods.map((good) => (
+			<GoodItem
+				id={good.id}
+				key={good.id}
+				name={good.name}
+				description={good.description}
+				price={good.price}
+			/>
+		));
 
   return (
     <section className={classes.goods}>
