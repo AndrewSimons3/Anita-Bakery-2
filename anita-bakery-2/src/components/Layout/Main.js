735550d88ed11ffbe66eb7df7ce1aae4acab39c6
AndrewSimons3 <simdrew07@gmail.com>
2021-12-10 18:48:36 -0600

    updated landing page

--- anita-bakery-2/src/components/Layout/Main.js
+++ anita-bakery-2/src/components/Layout/Main.js
@@ -8,7 +8,13 @@ const Main = () => {
 		<Fragment>
 			<section className={classes['section-hero']}>
 				<div className={classes['hero-left']}>
-					this is where the title and summary will go
+					<h1 className={classes.title}>Anita's</h1>
+					<h1 className={classes.title}>Bakery</h1>
+					<h2 className={classes.summary}>
+						All of our goods are cooked with high-quality ingredients, by our
+						experienced baker Anita!
+          </h2>
+          <button className={classes.button}>Shop Now</button>
 				</div>
 				<div className={classes['hero-right']}>
 					<img className={classes.store} src={store} alt='coffee shop' />
